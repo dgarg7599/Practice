19b7196576714c8e00f95648d89cacb1b4bc1b79
dgarg7599 <gargdivyansh7599@gmail.com>
2025-02-25 14:46:47 +0530

    [Divyansh Garg] mod: Change the Name

--- HelloDivyansh.java
+++ HelloDivyansh.java
@@ -1,5 +1,5 @@
 public class HelloDivyansh{
 	public static void main(String[] args){
-		System.out.print("Hello, My name is Divyansh Garg.");
+		System.out.print("Hello, My name is Divyansh.");
 	}
 }
